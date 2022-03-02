8b65c5c38ccadcf34635f7da6c9a6a1f78f4bcb9
weirdyang <wy4786@gmail.com>
2022-03-03 04:54:29 +0800

    handling exception badly

--- netlify/functions/twitter.js
+++ netlify/functions/twitter.js
@@ -4,37 +4,51 @@ const { TwitterApi } = require('twitter-api-v2');
 
 
 exports.handler = async function ({ queryStringParameters }) {
+
+  try {
+    const twitterClient = new TwitterApi(bearer);
+
+    // Tell typescript it's a readonly app
+    const roClient = twitterClient.readOnly;
+
+    // Play with the built in methods
+    const { q: username } = queryStringParameters;
+    if (!username) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(
+          {
+            error: 'no username'
+          }, null, 2)
+      }
+    }
+    const user = await roClient.v2.userByUsername(username,
+      {
+        'user.fields': 'profile_image_url'
+      });
+    user.data.profile_image_url
+    if (user.errors) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(
+          {
+            error: user.errors[0].detail
+          }, null, 2)
+      }
+    }
+    const result = { username, user }
+    return { statusCode: 200, body: JSON.stringify(result, null, 2) }
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          error: 'whoops'
+        }, null, 2
+      )
+    }
+  }
   // Instanciate with desired auth type (here's Bearer v2 auth)
-  const twitterClient = new TwitterApi(bearer);
 
-  // Tell typescript it's a readonly app
-  const roClient = twitterClient.readOnly;
-
-  // Play with the built in methods
-  const { q: username } = queryStringParameters;
-  if (!username) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(
-        {
-          error: 'no username'
-        }, null, 2)
-    }
-  }
-  const user = await roClient.v2.userByUsername(username,
-    {
-      'user.fields': 'profile_image_url'
-    });
-  user.data.profile_image_url
-  if(user.errors){
-    return {
-      statusCode: 500,
-      body: JSON.stringify(
-        {
-          error: user.errors[0].detail
-        }, null, 2)
-    }
-  }
-  const result = { username, user}
-  return { statusCode: 200, body: JSON.stringify(result, null, 2) }
 }
